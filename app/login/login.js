f7662b4c155855b1973ca901025907d6497f712e
MorningStar <792634614@qq.com>
2018-04-11 21:50:33 +0800

    update

---
+++ app/login/login.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+import { 
+    View,
+    TextInput,
+    Text,
+    TouchableHighlight,
+} from 'react-native';
---
+++ app/login/loginStyle.js
@@ -0,0 +1,5 @@
+import { 
+    StyleSheet,
+    Dimensions
+} from 'react-native';
+const win = Dimensions.get('window');
