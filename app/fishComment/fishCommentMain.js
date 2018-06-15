b72727a55891719ec4d981c280d08ab2e88658b4
MorningStar <792634614@qq.com>
2018-06-15 10:04:25 +0800

    update

--- app/fishComment/fishCommentMain.js
+++ app/fishComment/fishCommentMain.js
@@ -4,22 +4,24 @@ import {
     Text,
     TextInput,
     TouchableHighlight,
+    WebView
 } from 'react-native';
 import CommentListContainer from './commentListContainer'
 import { styles } from './styles/mainStyles';
 export default class FishComment extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
     }
     static navigationOptions = {
-        title:'测试一水族箱的社区'
+        title: '测试一水族箱的社区'
     }
     render() {
         return (
             <View style={styles.container} >
-                <CommentListContainer/>
+                
+                <CommentListContainer />
                 <View style={styles.speak}>
-                    <TextInput style={styles.commentInput} placeholder="发表你的看法"/>
+                    <TextInput style={styles.commentInput} placeholder="发表你的看法" />
                     <TouchableHighlight style={styles.submitButton}>
                         <Text>发送</Text>
                     </TouchableHighlight>
